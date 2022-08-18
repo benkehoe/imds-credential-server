2de09af836f6f340c8ab5deb1c490a1aca0a015e
Ben Kehoe <ben@kehoe.io>
2022-08-18 16:21:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# imds-credential-server
+**Provide AWS credentials to a container from the host**
+
+This CLI tool runs a server compliant with the [EC2 IMDSv2 interface](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/configuring-instance-metadata-service.html) in order to vend AWS credentials, primarily to export credentials into locally-run containers.
+
+This is better than mounting your `~/.aws` directory into a container as a) it allows for mechanisms that only work on the host, e.g., custom credential processes and b) it only vends one set of (refreshable) credentials to the container rather than providing access to all your credentials.
+
+## Quickstart
+
+Install from source or with `go install`.
+[`go install` will install to `$GOPATH/bin` or `$HOME/go/bin`](https://pkg.go.dev/cmd/go#hdr-Compile_and_install_packages_and_dependencies), so ensure that directory is on your `$PATH`.
+
+
+```bash
+$ go install github.com/benkehoe/imds-credential-server@main
+```
+
+Run the server, and then use it with a container:
+
+```
+# in one terminal
+$ imds-credential-server 8081
+
+# in a separate terminal
+# note the trailing slash on the URL
+$ docker run --rm -p 8081:8081 -e AWS_EC2_METADATA_SERVICE_ENDPOINT=http://host.docker.internal:8081/
+amazon/aws-cli sts get-caller-identity
+{
+    "UserId": "AROAXXXXXXXXXXXXXXXXX:SessionName",
+    "Account": "123456789012",
+    "Arn": "arn:aws:sts::123456789012:assumed-role/SomeRole/SessionName"
+}
+```
+
+## Details
+
+You must provide a port (or optionally a full address) for the server.
+Then map the port from the host to the container, and set the environment variable `AWS_EC2_METADATA_SERVICE_ENDPOINT` to `http://host.docker.internal:MAPPED_PORT/` with the approporiate port and **remember to include the trailing slash.**
+
+AWS SDKs run inside the container should just work.
+
+You can use `imds-credential-server version` to get the version.
---
+++ main.go
@@ -0,0 +1,328 @@
+// Copyright 2020 Ben Kehoe
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+var Version string = "0.1"
+
+type innerError struct {
+	Code    string
+	Message string
+}
+
+type errorBody struct {
+	Error innerError
+}
+
+func writeError(w http.ResponseWriter, status int, code, message string) {
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	errorBody := errorBody{
+		Error: innerError{
+			Code:    code,
+			Message: message,
+		},
+	}
+	bodyBytes, err := json.Marshal(errorBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(bodyBytes)
+}
+
+type Config struct {
+	secret        []byte
+	AwsConfig     aws.Config
+	PrincipalArn  string
+	PrincipalName string
+}
+
+func NewConfig(awsCfg aws.Config) *Config {
+	secretBytes := make([]byte, 32)
+	_, err := rand.Read(secretBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stsClient := sts.NewFromConfig(awsCfg)
+
+	resp, err := stsClient.GetCallerIdentity(context.TODO(), &sts.GetCallerIdentityInput{})
+	if err != nil {
+		log.Fatalf("Call to GetCallerIdentity failed, %v", err)
+	}
+	arn := resp.Arn
+	arnParts := strings.Split(*arn, ":")
+	nameParts := strings.Split(arnParts[len(arnParts)-1], "/")
+	nameType := nameParts[0]
+	var principalName string
+	if nameType == "user" {
+		principalName = nameParts[len(nameParts)-1]
+	} else {
+		principalName = nameParts[1]
+	}
+
+	return &Config{
+		secret:        secretBytes,
+		AwsConfig:     awsCfg,
+		PrincipalArn:  *arn,
+		PrincipalName: principalName,
+	}
+}
+
+func (cfg *Config) EncodeToken(ttl time.Duration) []byte {
+	now := time.Now().UTC()
+	expiration := now.Add(ttl)
+	expirationBytes, err := expiration.MarshalText()
+	if err != nil {
+		log.Fatal(err)
+	}
+	encodedExpirationStr := base64.URLEncoding.EncodeToString(expirationBytes)
+
+	mac := hmac.New(sha256.New, cfg.secret)
+	mac.Write(expirationBytes)
+
+	macBytes := mac.Sum(nil)
+	macStr := base64.URLEncoding.EncodeToString(macBytes)
+
+	token := make([]byte, 0, len(encodedExpirationStr)+len(macStr)+1)
+
+	token = append(token, encodedExpirationStr...)
+	token = append(token, "."...)
+	token = append(token, macStr...)
+
+	return token
+}
+
+func (cfg *Config) ValidateToken(token string) error {
+	parts := strings.Split(token, ".")
+	if len(parts) != 2 {
+		return errors.New("The IMDSv2 token is invalid")
+	}
+	encodedExpirationStr := parts[0]
+	macStr := parts[1]
+
+	expirationBytes, err := base64.URLEncoding.DecodeString(encodedExpirationStr)
+	if err != nil {
+		return errors.New("The IMDSv2 token is invalid")
+	}
+
+	var expiration time.Time
+	err = expiration.UnmarshalText(expirationBytes)
+	if err != nil {
+		return errors.New("The IMDSv2 token is invalid")
+	}
+
+	macBytes, err := base64.URLEncoding.DecodeString(macStr)
+	if err != nil {
+		return errors.New("The IMDSv2 token is invalid")
+	}
+
+	mac := hmac.New(sha256.New, cfg.secret)
+	mac.Write(expirationBytes)
+
+	expectedMacBytes := mac.Sum(nil)
+	if !hmac.Equal(expectedMacBytes, macBytes) {
+		return errors.New("The IMDSv2 token is invalid")
+	}
+
+	now := time.Now().UTC()
+	if expiration.Before(now) {
+		return errors.New("The IMDSv2 token has expired")
+	}
+
+	return nil
+}
+
+func (cfg *Config) handleTokenRequest(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPut {
+		writeError(w, http.StatusMethodNotAllowed, "MethodNotAllowed", "Token must be obtained with PUT")
+		return
+	}
+
+	forwardedFor := req.Header.Get("x-forwarded-for")
+	if forwardedFor != "" {
+		writeError(w, http.StatusUnauthorized, "InvalidHeader", "Token requests can't contain X-Forwarded-For")
+		return
+	}
+
+	ttlStr := req.Header.Get("x-aws-ec2-metadata-token-ttl-seconds")
+	if ttlStr == "" {
+		writeError(w, http.StatusUnauthorized, "MissingTTL", "The IMDSv2 token expiration header is missing")
+		return
+	}
+	ttlInt, err := strconv.Atoi(ttlStr)
+	if err != nil {
+		writeError(w, http.StatusUnauthorized, "InvalidTTL", "The IMDSv2 token expiration is invalid")
+		return
+	}
+	if ttlInt <= 0 || ttlInt > 21600 {
+		writeError(w, http.StatusUnauthorized, "InvalidTTL", "The IMDSv2 token expiration is invalid")
+		return
+	}
+	ttl := time.Second * time.Duration(ttlInt)
+
+	bodyBytes := cfg.EncodeToken(ttl)
+
+	w.Header().Add("Content-type", "text/plain")
+	w.Write(bodyBytes)
+}
+
+func (cfg *Config) handleRequest(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		writeError(w, http.StatusMethodNotAllowed, "MethodNotAllowed", "Method not allowed")
+		return
+	}
+
+	token := req.Header.Get("x-aws-ec2-metadata-token")
+	if token == "" {
+		writeError(w, http.StatusUnauthorized, "MissingToken", "The IMDSv2 token header is missing")
+		return
+	}
+	if err := cfg.ValidateToken(token); err != nil {
+		writeError(w, http.StatusUnauthorized, "InvalidToken", err.Error())
+		return
+	}
+
+	if req.URL.Path == "/latest/meta-data/iam/security-credentials/" {
+		cfg.handleRoleRequest(w, req)
+		return
+	} else {
+		role := req.URL.Path[len("/latest/meta-data/iam/security-credentials/"):]
+		cfg.handleCredentialRequest(w, req, role)
+	}
+}
+
+func (cfg *Config) handleRoleRequest(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-type", "text/plain")
+	io.WriteString(w, cfg.PrincipalName)
+}
+
+type Credentials struct {
+	AccessKeyId     string
+	SecretAccessKey string
+	Token           string `json:",omitempty"`
+	Expiration      string
+}
+
+func (cfg *Config) handleCredentialRequest(w http.ResponseWriter, req *http.Request, role string) {
+	awsCreds, err := cfg.AwsConfig.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		log.Println(err)
+		writeError(w, http.StatusInternalServerError, "InternalServerError", "Something went wrong")
+		return
+	}
+	creds := Credentials{
+		AccessKeyId:     awsCreds.AccessKeyID,
+		SecretAccessKey: awsCreds.SecretAccessKey,
+	}
+	if awsCreds.SessionToken != "" {
+		creds.Token = awsCreds.SessionToken
+	}
+	if !awsCreds.Expires.IsZero() {
+		expiration, err := awsCreds.Expires.MarshalText()
+		if err != nil {
+			log.Println(err)
+			writeError(w, http.StatusInternalServerError, "InternalServerError", "Something went wrong")
+			return
+		}
+		creds.Expiration = string(expiration)
+	} else {
+		expirationTime := time.Now().Add(time.Hour)
+		expiration, err := expirationTime.MarshalText()
+		if err != nil {
+			log.Println(err)
+			writeError(w, http.StatusInternalServerError, "InternalServerError", "Something went wrong")
+			return
+		}
+		creds.Expiration = string(expiration)
+	}
+
+	bodyBytes, err := json.Marshal(creds)
+	if err != nil {
+		log.Println(err)
+		writeError(w, http.StatusInternalServerError, "InternalServerError", "Something went wrong")
+		return
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	w.Write(bodyBytes)
+}
+
+/*
+PUT /latest/api/token -> token
+GET /latest/meta-data/iam/security-credentials/ -> role name
+GET /latest/meta-data/iam/security-credentials/{role_name} -> creds
+*/
+func (cfg *Config) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Println(req.Method, req.URL.Path)
+	if req.URL.Path == "/latest/api/token" {
+		cfg.handleTokenRequest(w, req)
+	} else if strings.HasPrefix(req.URL.Path, "/latest/meta-data/iam/security-credentials/") {
+		cfg.handleRequest(w, req)
+	} else {
+		writeError(w, http.StatusNotFound, "InvalidPath", "Invalid path")
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: Port not specified")
+		os.Exit(1)
+	}
+
+	spec := os.Args[1]
+	if spec == "version" {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
+	_, err := strconv.Atoi(spec)
+	if err == nil {
+		spec = ":" + spec
+	}
+
+	rand.Seed(time.Now().UnixMicro())
+
+	awsConfig, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := NewConfig(awsConfig)
+
+	fmt.Printf("Identity: %s\n", config.PrincipalArn)
+
+	log.Fatal(http.ListenAndServe(spec, config))
+}
